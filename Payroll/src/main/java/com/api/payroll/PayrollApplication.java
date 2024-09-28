b475fcfb96b1ccaa686a6ea9332728e1deedaffa
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-28 20:03:08 -0300

    Feign client

--- Payroll/src/main/java/com/api/payroll/PayrollApplication.java
+++ Payroll/src/main/java/com/api/payroll/PayrollApplication.java
@@ -2,7 +2,9 @@ package com.api.payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class PayrollApplication {
 
--- Payroll/src/main/java/com/api/payroll/services/PaymentService.java
+++ Payroll/src/main/java/com/api/payroll/services/PaymentService.java
@@ -1,13 +1,25 @@
 package com.api.payroll.services;
 
+import com.api.payroll.WorkerFeignClient;
 import com.api.payroll.entities.Payment;
+import com.api.payroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentService {
 
+    @Autowired
+    private WorkerFeignClient workerFeignClient;
+
     public Payment getPayment(long workerId, int days) {
 
-        return new Payment("Bob", 200.0, days);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+
+        if (worker == null) {
+            return null;
+        }
+
+        return new Payment(worker.getName(),  worker.getDailyIncome(), days);
     }
 }
