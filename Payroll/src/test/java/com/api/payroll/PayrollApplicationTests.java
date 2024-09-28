3f9e6d090680c951ff0c3307fc2736099eb98207
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-28 14:25:23 -0300

    Project payroll initial commit

---
+++ Payroll/src/test/java/com/api/payroll/PayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
