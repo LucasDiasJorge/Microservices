74946886f224a52404cba75f8fc3500f6328e69e
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-09-28 23:09:45 -0300

    Random port

--- Workers/src/main/java/com/api/worker/WorkerApplication.java
+++ Workers/src/main/java/com/api/worker/WorkerApplication.java
@@ -1,15 +1,29 @@
 package com.api.worker;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class WorkerApplication {
 
-	public static void main(String[] args) {
+	private final Environment environment;
+
+    public WorkerApplication(Environment environment) {
+        this.environment = environment;
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(WorkerApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		String port = environment.getProperty("server.port");
+		System.out.println("Running on port: " + port);
+	}
 }
