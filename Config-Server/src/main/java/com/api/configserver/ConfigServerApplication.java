b42f77be4c049b3ba3a40190dc9270643d30c01b
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-02 16:11:48 -0300

    Implementing get of properties

--- Config-Server/src/main/java/com/api/configserver/ConfigServerApplication.java
+++ Config-Server/src/main/java/com/api/configserver/ConfigServerApplication.java
@@ -2,7 +2,9 @@ package com.api.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
 
