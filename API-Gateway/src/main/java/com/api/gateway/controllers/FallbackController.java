a6c10433138f351cb06a645d8d9bf8514f35ad9a
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-01 23:36:53 -0300

    Melhorando legibilidade do fallback

--- API-Gateway/src/main/java/com/api/gateway/controllers/FallbackController.java
+++ API-Gateway/src/main/java/com/api/gateway/controllers/FallbackController.java
@@ -1,5 +1,6 @@
 package com.api.gateway.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ public class FallbackController {
 
     @GetMapping("/fallback")
     public ResponseEntity<String> fallback() {
-        return ResponseEntity.ok("Serviço temporariamente indisponível. Tente novamente mais tarde.");
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body("{\"message\": \"Serviço Indisponível\"}");
     }
 }
