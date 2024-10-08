33dd4e30a0e37aa534709faa392ffef9f3d083a2
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-08 12:49:44 -0300

    OAuth project created

---
+++ OAuth/src/test/java/com/api/oauth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
