1cb6c0e6b02eba465755163a15a3b03945a6ccd4
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-11 11:44:05 -0300

    Allowing /public and /auth route without authorization

--- OAuth/src/main/java/com/api/oauth/config/SecurityConfig.java
+++ OAuth/src/main/java/com/api/oauth/config/SecurityConfig.java
@@ -2,20 +2,24 @@ package com.api.oauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
+                .csrf().disable() // porra
                 .authorizeHttpRequests(
                         authorizeRequests -> {
-                            authorizeRequests.requestMatchers("/public").permitAll();
-                            authorizeRequests.requestMatchers("/auth").permitAll();
+                            authorizeRequests.requestMatchers(HttpMethod.GET,"/public").permitAll();
+                            authorizeRequests.requestMatchers(HttpMethod.POST,"/auth").permitAll();
                             authorizeRequests.anyRequest().authenticated();
                         }
                 ).oauth2ResourceServer(conf -> conf.jwt(Customizer.withDefaults()))
