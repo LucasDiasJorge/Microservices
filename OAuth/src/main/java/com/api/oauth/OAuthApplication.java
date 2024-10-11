c0514adcc86cc3ea400985afca21ef3b13d9b51e
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-11 11:08:28 -0300

    Removing feign client

--- OAuth/src/main/java/com/api/oauth/OAuthApplication.java
+++ OAuth/src/main/java/com/api/oauth/OAuthApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 public class OAuthApplication implements CommandLineRunner {
 
 	@Autowired
