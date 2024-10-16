94b136ade7cb74a2b2a84af299af5653d9507422
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-16 16:33:41 -0300

    Create AccessLoggingFilter.java

---
+++ API-Gateway/src/main/java/com/api/gateway/logger/AccessLoggingFilter.java
@@ -0,0 +1,43 @@
+package com.api.gateway.logger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class AccessLoggingFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccessLoggingFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+
+        String method = httpRequest.getMethod();
+        String requestURI = httpRequest.getRequestURI();
+        String remoteAddr = request.getRemoteAddr();
+
+        logger.info("Acesso: {} {} - IP: {}", method, requestURI, remoteAddr);
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
