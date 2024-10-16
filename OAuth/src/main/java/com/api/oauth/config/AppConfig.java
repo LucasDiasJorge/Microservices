b942304312c993b3838b40227d3230feb2322345
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-16 16:34:05 -0300

    Refactoring

--- OAuth/src/main/java/com/api/oauth/config/AppConfig.java
+++ OAuth/src/main/java/com/api/oauth/config/AppConfig.java
@@ -5,16 +5,10 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
     @Bean
     public RestTemplate restTemplate() {
