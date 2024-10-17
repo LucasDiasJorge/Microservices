5bf80e1da8578a2e13f687879009bd4a10ab45a1
Lucas Dias Jorge <78762601+LucasDiasJorge@users.noreply.github.com>
2024-10-17 16:05:57 -0300

    Fixing public route access

--- API-Gateway/src/main/java/com/api/gateway/security/SecurityConfig.java
+++ API-Gateway/src/main/java/com/api/gateway/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.api.gateway.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
@@ -17,9 +18,11 @@ public class SecurityConfig {
         return http
                 .authorizeExchange(
                         exchange -> {
-                            exchange.pathMatchers(HttpMethod.POST, "/oauth/auth").permitAll().anyExchange().authenticated();
+                            exchange.pathMatchers(HttpMethod.POST, "/oauth/auth").permitAll();
+                            exchange.pathMatchers(HttpMethod.GET, "/oauth/public").permitAll();
+                            exchange.anyExchange().authenticated();
                         }
-                )
+                ).oauth2ResourceServer(conf -> conf.jwt(Customizer.withDefaults()))
                 .build();
     }
 
